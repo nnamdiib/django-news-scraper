51e675c3fee6736f32df0b3e2d4a1433439ff9f7
Nnamdi <nnamdiibns@gmail.com>
2018-01-19 23:51:37 +0100

    Modify celery and tasks

--- celery.py
+++ celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
  
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news.settings')
  
@@ -8,3 +9,10 @@ app.config_from_object('django.conf:settings')
  
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+  'scrape-every-10-mins': {
+    'task': 'publisher.tasks.send_view_count_report',
+    'schedule': crontab(minute='*/10')
+  },
+}
