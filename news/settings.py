eab845e84570a99f4b94dac1a0cabb46b628f86d
Nnamdi <nnamdiibns@gmail.com>
2018-01-20 18:25:32 +0100

    Set database password to environment variable

--- news/settings.py
+++ news/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'news',
         'USER': 'nnamdi',
-        'PASSWORD': 'root',
+        'PASSWORD': os.environ.get('NEWS_APP_DB_PASS'),
         'HOST': 'localhost',
         'PORT': '',
     }
